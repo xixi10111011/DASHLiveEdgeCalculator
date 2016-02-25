834bfa512a60c3ad7b9ca8915e641836c7f9e6ab
xixi10111011 <xixi10111011@gmail.com>
2016-02-25 16:30:19 +0800

    add a start-to-play-time optional arg
    add new feature to support calculate edge segment at a given dedicated time.

--- src/CalcLiveEdge.java
+++ src/CalcLiveEdge.java
@@ -11,21 +11,25 @@ public class CalcLiveEdge
 
     public static void main(String[] args)
     {
-        System.out.println("CalcLiveEdge 1.0");
+        System.out.println("CalcLiveEdge 1.1");
         System.out.println("");
         
-        if (args.length < 4)
+        if (args.length != 4 && args.length != 5)
         {
-            System.out.println("Usage: CalcLiveEdge availabilityStartTime duration timescale startNumber");
-            System.out.println("Example: CalcLiveEdge 2015-03-30T09:30:10Z 60060000 10000000 1427139101");
+            System.out.println("Usage: CalcLiveEdge availabilityStartTime duration timescale startNumber [start-to-play-time]");
+            System.out.println("Example: CalcLiveEdge 2015-03-30T09:30:10Z 60060000 10000000 1427139101 [20150412T192050]");
             System.out.println("");
             System.out.println("Copyright (C) 2015 bitmovin GmbH");
+            System.out.println("Copyright (C) 2016 https://github.com/xixi10111011");
             
             return;
         }
 
         DateFormat  dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        
+        DateFormat  dateFormatEx = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+        dateFormatEx.setTimeZone(TimeZone.getTimeZone("UTC"));
         
         Date availabilityStartTime;
         try
@@ -37,7 +41,22 @@ public class CalcLiveEdge
             return;
         }
         
-        Date    now                 = Calendar.getInstance().getTime();
+        Date now;
+        if (args.length == 4)
+        {
+            now = Calendar.getInstance().getTime();
+        }
+        else
+        {
+            try
+            {
+                now = dateFormatEx.parse(args[4]);
+            } catch (ParseException e)
+            {
+                System.out.println("Your start-to-play-time ist not in the following format: YYYYMMDDTHHmmss");
+                return;
+            }
+        }
         long    secondsDifference   = (now.getTime() - availabilityStartTime.getTime()) / 1000;
         long    segmentDuration     = Long.parseLong(args[1]);
         long    segmentTimescalce   = Long.parseLong(args[2]);
